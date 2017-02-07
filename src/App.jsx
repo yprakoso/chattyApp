4b49efd567f270b5eb9f41ec0b4b1b6d8577eb33
Yoga Prakoso <yogaprakoso90@gmail.com>
2017-02-07 00:03:45 +0000

    first  react component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList />
+        <ChatBar />
+      </div>
     );
   }
 }
